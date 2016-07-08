3d8cb5720811e0b63ff84abefceb0dbdc9204057
JornDallinga <jorndallinga@gmail.com>
2016-07-08 15:13:43 +0200

    added parameter on start_date

--- R/timeStackProbaV2.R
+++ R/timeStackProbaV2.R
@@ -1,4 +1,4 @@
-timeStackProbaV2 <- function(x, pattern, order_chrono=TRUE, tile=NULL, quick=FALSE, end_date=NULL, ...){
+timeStackProbaV2 <- function(x, pattern, order_chrono=TRUE, tile=NULL, quick=FALSE, end_date=NULL, start_date=NULL, ...){
   
   df_info <- getProbaVinfo(x, pattern)
   if(order_chrono){
@@ -6,7 +6,15 @@ timeStackProbaV2 <- function(x, pattern, order_chrono=TRUE, tile=NULL, quick=FAL
   }
   
   if (!is.null(tile)) df_info  <- df_info[df_info$tile==tile, ]
-  if (!is.null(end_date)) df_info  <- df_info[as.numeric(df_info$date) <= end_date,]
+  if (!is.null(end_date) & !is.null(start_date)){
+    df_info  <- df_info[(df_info$date) <= end_date & (df_info$date) >= start_date,]
+  } else if (is.null(end_date)) {
+      end_date <- max(df_info$date)
+  } else if (is.null(start_date)){
+      start_date <- min(df_info$date)
+  }
+  
+  if (!is.null(end_date) & !is.null(start_date)) df_info  <- df_info[(df_info$date) <= end_date & (df_info$date) >= start_date,]
   
   
   s <- raster::stack(file.path(fdir, df_info$fpath), quick=quick)
